2116236db8c638ba73cd7971dc02747c44e9f810
Phini0n <patrayowilson@gmail.com>
2024-10-25 13:21:34 -0400

    Added DealershipFileManager

---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+
+    public DealershipFileManager() {}
+
+    public Dealership getDealership() {
+        return null;
+    }
+
+    public Dealership saveDealership() {
+        return null;
+    }
+
+}
