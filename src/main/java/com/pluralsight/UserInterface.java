cdd2492b30a6293982acf3f8fc30b0542504d714
Phini0n <patrayowilson@gmail.com>
2024-10-25 14:41:51 -0400

    Created UserInterface class

---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,44 @@
+package com.pluralsight;
+
+public class UserInterface {
+    private Dealership dealership;
+
+    public void display() {
+
+    }
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processGetAllVehicleRequest() {
+
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest(){
+
+    }
+}
